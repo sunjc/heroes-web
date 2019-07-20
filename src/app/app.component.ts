257c0938714bfda7c61803a75ff4dd40039ccefe
Sunjc <sun-coco925>
2019-07-20 18:38:53 +0800

    i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Inject, LOCALE_ID} from '@angular/core';
+import {en_US, NzI18nService, zh_CN} from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  currentLanguage: string;
+  currentDate: Date = new Date();
+
+  supportLanguages = [
+    {code: 'en', label: 'English'},
+    {code: 'zh', label: '中文'}
+  ];
+
+  constructor(@Inject(LOCALE_ID) private localeId: string, private i18n: NzI18nService) {
+    if (localeId.startsWith('en')) {
+      this.currentLanguage = 'en';
+      this.i18n.setLocale(en_US);
+    } else {
+      this.currentLanguage = 'zh';
+      this.i18n.setLocale(zh_CN);
+    }
+  }
 }
