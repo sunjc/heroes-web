9897ddc449a15b8cfbc5ab1921486c83bb214eb6
sunjc <sunjc@iata.org>
2020-02-11 14:23:52 +0800

    set document title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, LOCALE_ID} from '@angular/core';
 import {en_US, NzI18nService, zh_CN} from 'ng-zorro-antd';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +17,15 @@ export class AppComponent {
     {code: 'zh', label: '中文'}
   ];
 
-  constructor(@Inject(LOCALE_ID) private localeId: string, private i18n: NzI18nService) {
+  constructor(@Inject(LOCALE_ID) private localeId: string, private i18n: NzI18nService, private titleService: Title) {
     if (localeId === 'en-US') {
       this.selectedLanguage = 'en';
       this.i18n.setLocale(en_US);
+      this.titleService.setTitle('Tour of Heroes');
     } else {
       this.selectedLanguage = 'zh';
       this.i18n.setLocale(zh_CN);
+      this.titleService.setTitle('英雄之旅');
     }
   }
 
