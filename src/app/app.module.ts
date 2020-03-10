8cc3104ccb23519a900873c786e665ac7d1a90f7
sunjc <sunjc@iata.org>
2020-03-10 20:49:37 +0800

    format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {en_US, NgZorroAntdModule, NZ_CONFIG, NZ_I18N, NzConfig} from 'ng-zorro-antd';
+
+import {registerLocaleData} from '@angular/common';
+import en from '@angular/common/locales/en';
 
 import {AppRoutingModule} from './app-routing.module';
-
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
@@ -12,13 +16,9 @@ import {HeroesComponent} from './heroes/heroes.component';
 import {HeroSearchComponent} from './hero-search/hero-search.component';
 import {MessagesComponent} from './messages/messages.component';
 import {LoginComponent} from './login/login.component';
+import {PaginationComponent} from './pagination/pagination.component';
 import {AuthenticationInterceptor} from './authentication.interceptor';
 import {HasRoleDirective} from './has-role.directive';
-import {en_US, NgZorroAntdModule, NZ_CONFIG, NZ_I18N, NzConfig} from 'ng-zorro-antd';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {registerLocaleData} from '@angular/common';
-import en from '@angular/common/locales/en';
-import {PaginationComponent} from './pagination/pagination.component';
 
 registerLocaleData(en);
 
