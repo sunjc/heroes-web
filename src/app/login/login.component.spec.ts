c20de359c783baf59ad8c46011259f631247ae71
Sun Jingchuan <sjc-925@163.com>
2020-11-26 11:07:00 +0800

    upgrade: bump ng-zorro-antd to 10.1

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,14 +2,11 @@ import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {RouterTestingModule} from '@angular/router/testing';
-import {
-  NzButtonModule,
-  NzCheckboxModule,
-  NzFormModule,
-  NzIconModule,
-  NzInputModule,
-  NzOutletModule
-} from 'ng-zorro-antd';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {NzCheckboxModule} from 'ng-zorro-antd/checkbox';
+import {NzFormModule} from 'ng-zorro-antd/form';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzInputModule} from 'ng-zorro-antd/input';
 import {of} from 'rxjs/internal/observable/of';
 import {LoginComponent} from './login.component';
 import {AuthenticationService} from '../authentication.service';
@@ -34,7 +31,6 @@ describe('LoginComponent', () => {
         NzFormModule,
         NzIconModule,
         NzInputModule,
-        NzOutletModule,
         HttpClientTestingModule,
         RouterTestingModule.withRoutes([])
       ],
